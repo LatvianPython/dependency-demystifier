e0d627b04a6d8a64ebd40f61e1316361f08cb5c9
LatvianPython <kalvans.rolands@gmail.com>
2019-01-10 00:20:10 +0200

    initial implementation of searching for dependencies directly with issue key

--- slack_integration.py
+++ slack_integration.py
@@ -3,6 +3,7 @@ import svn.exception
 import configparser
 import keyring
 import logging
+import re
 from logging.config import fileConfig
 from contextlib import suppress
 from getpass import getpass
@@ -29,13 +30,16 @@ def main():
                              password=getpass('Input jira password for {}: '.format(username)))
         password = keyring.get_password(service_name=service_name, username=username)
 
-    issue_regex = config['SVN']['issuekey_regex']
+    issue_regex = re.compile(config['SVN']['issuekey_regex'])
+
     file_extensions = config['SVN']['accepted_extensions'].split(',')
     working_copy_path = config['SVN']['working_copy_path']
+    dev_branch = config['SVN']['dev_branch']
 
     dependency_checker = DependencyChecker(jira=(server, username, password),
                                            svn_working_copy_path=working_copy_path, extensions_to_check=file_extensions,
-                                           statuses_to_ignore=statuses_to_ignore, issue_regex=issue_regex)
+                                           statuses_to_ignore=statuses_to_ignore, issue_regex=issue_regex,
+                                           dev_branch=dev_branch)
 
     slack = SlackClient(config['SLACK']['token'])
 
@@ -49,16 +53,34 @@ def main():
                             logger.info('got request from:"{}" for "{}"'.format(event['user'], event['text']))
                             logger.debug(event)
                             try:
-                                revision = int(event['text'])
-                                dependencies = dependency_checker.get_dependencies_as_dict(revision)
-                                dependency_summary = format_as_slack_attachment(dependencies, server)
+                                # fixme: don't like the look of this piece of code, could say the same for whole file
+                                issues = issue_regex.findall(event['text'])
+                                if len(issues) >= 5:
+                                    logger.warning('USER:"{}" tried to get summary for {} issued'.format(event['user'],
+                                                                                                         len(issues)))
+                                    slack.api_call("chat.postMessage",
+                                                   channel=event['channel'],
+                                                   text='Too many issues in one call! Do less than 5 :x:')
+                                    continue
+                                elif len(issues) > 0:
+                                    dependency_summary = []
+                                    for issue in issues:
+                                        dependencies = dependency_checker.get_dependencies_as_dict(issue_key=issue)
+                                        dependency_summary += format_as_slack_attachment(dependencies=dependencies,
+                                                                                         jira_server=server)
+                                else:
+                                    revision = int(event['text'])
+                                    dependencies = dependency_checker.get_dependencies_as_dict(
+                                        revision_to_check=revision)
+                                    dependency_summary = format_as_slack_attachment(dependencies=dependencies,
+                                                                                    jira_server=server)
                                 logger.debug(dependency_summary)
                                 slack.api_call("chat.postMessage",
                                                channel=event['channel'],
                                                attachments=dependency_summary)
                                 logger.info('success!')
                             except ValueError:
-                                logger.warning('ValueError by {}'.format(event['user']))
+                                logger.warning('USER:"{}" tried to enter bad revision number'.format(event['user']))
                                 slack.api_call("chat.postMessage",
                                                channel=event['channel'],
                                                text='Enter just a plain revision number! :x:')
@@ -69,7 +91,7 @@ def main():
                                                    channel=event['channel'],
                                                    text='No such revision! :x:')
                                 else:
-                                    logger.error('Unknown SvnException', exc_info=1)
+                                    logger.error('USER:"{}" Unknown SvnException'.format(event['user']), exc_info=1)
                                     raise
 
             # slack does not allow more than 1 message post per sec
